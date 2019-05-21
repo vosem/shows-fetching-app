80e6d9246ac3842241cef367f3ae8814b03fbf37
Anna <anna.v.popovska@gmail.com>
2019-05-21 14:18:17 +0300

    Updates

---
+++ precache-manifest.cc99bd3a82af99828493649f1499d2b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbdf4095288db981b4c806ca316d2ea",
+    "url": "/shows-fetching-app/index.html"
+  },
+  {
+    "revision": "88d07e2fe4f3df50f46d",
+    "url": "/shows-fetching-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/shows-fetching-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "88d07e2fe4f3df50f46d",
+    "url": "/shows-fetching-app/static/js/main.06694012.chunk.js"
+  },
+  {
+    "revision": "7e3812b89caa67f5038b",
+    "url": "/shows-fetching-app/static/js/runtime~main.a32257cd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/shows-fetching-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
